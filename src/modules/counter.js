43c759842838471226d4c5472c8a4654549bd262
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-10-05 23:56:49 +0900

    [redux-actions] 리듀서 함수 더 간편하게 생성

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 // #. 액션 타입 정의 : '모듈명/액션명' 형태로 하여 액션명 충돌 방지
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -16,7 +16,7 @@ const initialState = {
 };
 
 // #. 리듀서 함수
-function counter(state = initialState, action) {
+/*function counter(state = initialState, action) {
   switch (action.type) {
     case INCREASE:
       return {
@@ -31,6 +31,14 @@ function counter(state = initialState, action) {
     default:
       return state;
   }
-}
+}*/
+// #. 리듀서 함수 redux-actions 로 더 간편하게 생성 : handleActions(<각 액션 업데이트 함수>, 초기 상태)
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
