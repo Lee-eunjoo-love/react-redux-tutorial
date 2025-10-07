322e3d6ce7574bdbb028d8cddf3a8fd666ff8177
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-10-07 12:29:27 +0900

    [immer] produce 사용하기

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import { produce } from 'immer';
 // #. 액션 타입 정의 : '모듈명/액션명' 형태로 하여 액션명 충돌 방지
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -109,7 +110,7 @@ const initialState = {
 //    createAction으로 만든 액션 생성함수는 파라미터로 받아 온 값을 객체 안에 넣을 때 payload 라는 이름으로 넣어 주므로
 //    action.payload 로 접근해야 한다.
 //    객체 비구조화 할당 문법으로 action 값의 payload 이름을 새로 설정하면 action.payload 가 어떤 값을 의미하는지 파악 용이.
-const todos = handleActions(
+/*const todos = handleActions(
   {
     [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
     [INSERT]: (state, { payload: text }) => ({
@@ -135,6 +136,47 @@ const todos = handleActions(
     }),
   },
   initialState,
+);*/
+
+// #. immer 복잡한 구조의 객체나 배열의 상태 관리 : immer 의 produce 사용
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: text }) =>
+      produce(state, (draft) => {
+        draft.todos.push({
+          id:
+            state.todos
+              .map((item) => item.id)
+              .reduce((max, current) => Math.max(max, current), 0) + 1,
+          text,
+          done: false,
+        });
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        if (todo) {
+          todo.done = !todo.done;
+        }
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        if (index !== -1) {
+          draft.todos.splice(index, 1);
+        }
+      }),
+  },
+  initialState,
 );
 
 export default todos;
+
+/**
+ * immer : 복잡한 구조의 객체나 배열의 상태 관리
+ *         (일반 자바스크립트로 처리하는 것이 더 편리한 경우 immer 적용은 선택)
+ */
